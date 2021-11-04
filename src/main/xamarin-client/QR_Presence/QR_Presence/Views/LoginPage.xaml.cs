f5a985f06ef6274478fb7449ffcfa06c0e8b6f33
CristiSandu <crystisandu3@gmail.com>
2021-11-04 10:54:24 +0200

    Changes:  - create view Profile Page  - make navigation page

--- src/main/xamarin-client/QR_Presence/QR_Presence/Views/LoginPage.xaml.cs
+++ src/main/xamarin-client/QR_Presence/QR_Presence/Views/LoginPage.xaml.cs
@@ -16,5 +16,10 @@ namespace QR_Presence.Views
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new ProfilePage());
+        }
     }
 }
