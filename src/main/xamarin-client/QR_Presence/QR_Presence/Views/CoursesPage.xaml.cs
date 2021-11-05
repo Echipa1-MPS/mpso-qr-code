9dc2af95969a9140deba90ba845560fb173ab1ef
CristiSandu <crystisandu3@gmail.com>
2021-11-05 10:25:53 +0200

    Changes:  - add top bar  - place ui elements as in design  - add new style for collection view chell

--- src/main/xamarin-client/QR_Presence/QR_Presence/Views/CoursesPage.xaml.cs
+++ src/main/xamarin-client/QR_Presence/QR_Presence/Views/CoursesPage.xaml.cs
@@ -12,9 +12,35 @@ namespace QR_Presence.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CoursesPage : ContentPage
     {
+
+        public List<Models.CourseModel> CourseList { get; set; } = new List<Models.CourseModel>
+        {
+            new Models.CourseModel
+            {
+                Name="MPS",
+                Prof="vasile",
+                Desc="",
+                Gradings=""
+            },
+            new Models.CourseModel
+            {
+                Name="EP",
+                Prof="Vali",
+                Desc="",
+                Gradings=""
+            },
+            new Models.CourseModel
+            {
+                Name="SADAS",
+                Prof="Valirwerwe",
+                Desc="",
+                Gradings=""
+            }
+        };
         public CoursesPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
     }
 }
