23f1f5fb7a90216dc293b1318296682ca85d39a6
CristiSandu <crystisandu3@gmail.com>
2021-11-10 00:10:08 +0200

    Changes:  - add button for edit and backbutton in Course page

--- src/main/xamarin-client/QR_Presence/QR_Presence/Views/Course/CoursePage.xaml.cs
+++ src/main/xamarin-client/QR_Presence/QR_Presence/Views/Course/CoursePage.xaml.cs
@@ -397,5 +397,15 @@ namespace QR_Presence.Views
 
             return;
         }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new Views.RegisterPage());
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
     }
 }
