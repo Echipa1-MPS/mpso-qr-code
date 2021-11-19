4adea0d92ff452c38cbd7b05866f651045ed36f3
Stefan <stefan.popa@equilobe.com>
2021-11-19 09:31:09 +0200

    Add react dropdowns on the CreateQR page

--- src/main/react-client/src/components/CreateQR/CreateQR.js
+++ src/main/react-client/src/components/CreateQR/CreateQR.js
@@ -1,17 +1,37 @@
 import { useEffect, useState, useContext } from "react";
 import { ThemeContext } from "../../App";
 import QRCode from "react-qr-code";
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
+import { getCoursesDetails } from "../../helpers/apicaller";
 
 export default function CreateQR() {
 
     const [courses, setCourses] = useState([]);
     const [courseIntervals, setCourseIntervals] = useState([]);
+    const [chosenCourse, setChosenCourse] = useState();
+    const [chosenInterval, setChosenInterval] = useState();
     const [qrDuration, setQrDuration] = useState(0);
     const [qrRepeats, setQrRepeats] = useState(0);
 
     const theme = useContext(ThemeContext);
 
-    const submitQrSession = () => {}
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const result = await getCoursesDetails();
+            setCourses(result);
+        }
+        fetchCourses();
+    }, []);
+
+
+    const submitQrSession = () => {
+        console.log("Afisam valorile");
+        console.log(chosenCourse);
+        console.log(chosenInterval);
+        console.log(qrDuration);
+        console.log(qrRepeats); 
+    }
 
     return (
         <div className="flex-container-row-center">
@@ -19,36 +39,57 @@ export default function CreateQR() {
                 <div className="aliceblue-background 
                                 qrsection-sections-width 
                                 qrsection-section-page-height">
-                    <div className="flex-container-column-center">  
+                    <div className="flex-container-column-center qr-section-container">  
                         <div className="flex-container-column" >
-                            <div>Alege materia</div>
-                            <div className="dropdown show">
-                                <span className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Materie</span>
-
-                                <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                    {['apa', 'tuica'].map(item => <span className="dropdown-item">{item}</span>)}
-                                </div>
-                            </div>
+                            <div className="qr-input-header">Choose subject</div>
+                            <DropdownButton
+                                alignRight
+                                title="Subject"
+                                id="dropdown-menu-align-right"
+                                onSelect={(e) => setChosenCourse(e)}
+                                    >
+                                        <Dropdown.Item eventKey="option-1">option-1</Dropdown.Item>
+                                        <Dropdown.Item eventKey="option-2">option-2</Dropdown.Item>
+                                        <Dropdown.Item eventKey="option-3">option 3</Dropdown.Item>
+                            </DropdownButton>
                         </div>
                         <div className="flex-container-column" style={{marginTop: "20px"}}>
-                            <div>Alege intervalul</div>
-                            <div className="dropdown show">
-                                <span className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Intervalul</span>
-
-                                <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                    {['apa', 'tuica'].map(item => <span className="dropdown-item">{item}</span>)}
-                                </div>
-                            </div>
+                            <div className="qr-input-header">Choose interval</div>
+                            <DropdownButton
+                                alignRight
+                                title="Interval"
+                                id="dropdown-menu-align-right"
+                                onSelect={(e) => setChosenInterval(e)}
+                                    >
+                                        <Dropdown.Item eventKey="option-1">option-1</Dropdown.Item>
+                                        <Dropdown.Item eventKey="option-2">option-2</Dropdown.Item>
+                                        <Dropdown.Item eventKey="option-3">option 3</Dropdown.Item>
+                            </DropdownButton>
                         </div>
-                        <div className="flex-container-row" style={{marginTop: "20px"}}>
-                            <div>Durata QR</div>
+                        <div className="flex-container-column" style={{marginTop: "20px"}}>
+                            <div className="qr-input-header">QR Duration</div>
+                            <input type="number"
+                                value = {qrDuration}
+                                onChange = {(e) => setQrDuration(e.target.value)}
+                                className="form-control" 
+                                id="qrDuration" 
+                                aria-describedby="qrDuration" 
+                                placeholder="Enter in seconds how much should the QR be available"/>
                         </div>
-                        <div className="flex-container-row" style={{marginTop: "20px"}}>
-                            <div>Repetare</div>
+                        <div className="flex-container-column" style={{marginTop: "20px"}}>
+                            <div className="qr-input-header">Repetition times</div>
+                            <input type="number"
+                                value = {qrRepeats}
+                                onChange = {(e) => setQrRepeats(e.target.value)}
+                                className="form-control" 
+                                id="qrDuration" 
+                                aria-describedby="qrDuration" 
+                                placeholder="Enter in seconds how much should the QR be available"/>
                         </div>
                         <div className="white-text-font
                                         create-qr-button-style 
                                         create-qr-button-width"
+                            onClick={submitQrSession}
                             style={{backgroundColor: theme.rose_budget}}>Submit</div>
                     </div>
 
--- src/main/react-client/src/helpers/apicaller.js
+++ src/main/react-client/src/helpers/apicaller.js
@@ -1,36 +1,32 @@
+import {
+    getCoursesBriefMock,
+    getCoursesDetailsMock,
+    getUpcomingCoursesMock,
+}
+from './mockup';
+
 export const apiHostUrl = '';
 export const postCreateQrUrl = `${apiHostUrl}/api/create-qr`;
 export const postUpdateQrUrl = `${apiHostUrl}/api/update-qr`;
 export const getHomeCoursesUrl = `${apiHostUrl}/api/courses`;
 export const getHomeProfileUrl = `${apiHostUrl}/api/profile`;
 export const getCoursesBriefUrl = `${apiHostUrl}/api/courses/brief`;
-export const getCourseDetailsUrl = `${apiHostUrl}/api/course/details`;
+export const getCoursesDetailsUrl = `${apiHostUrl}/api/course/details`;
 export const getUpcomingCoursesUrl = `${apiHostUrl}/api/upcoming/courses`;
 
-export function getCourses(params, success, failure) {
+export function getCoursesBrief(params, success, failure) {
+    return getCoursesBriefMock;
+}
 
-    return ['EP', 'APP', 'SPRC'];
-    //return [];
-    // axios.get(getHomeCoursesUrl, { params })
-    //     .then(success)
-    //     .catch(failure);
+export function getCoursesDetails(params, success, failure) {
+    return getCoursesDetailsMock;
 }
 
 export function getProfile(params, success, failure) {
     return { name: 'Stefan Popa', email: 'stefan.popa@gmail.com' };
-    // axios.get(getHomeProfileUrl, { params })
-    //     .then(success)
-    //     .catch(failure);
 }
 
 export function getUpcomingCourses(params, success, failure) {
-    return [
-        { name: "MPS", interval: "marti 16.00-18.00"},
-        { name: "EP", interval: "marti 18:00-20:00"},
-        { name: "APP", interval: "miercuri 10:00-12:00"},
-    ];
-    // axios.get(getUpcomingCoursesUrl, { params })
-    //     .then(success)
-    //     .catch(failure);
+    return getUpcomingCoursesMock;
 }
 
